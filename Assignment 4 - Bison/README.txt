7e480c84e3278fd0db1e9d7086c85ae309ff8f32
dope-dependent <rajas.bhatt@iitkgp.ac.in>
2021-10-17 12:24:34 +0530

    Added Assignment 4 - Bison

---
+++ Assignment 4 - Bison/README.txt
@@ -0,0 +1,2 @@
+'make' command generates a.out
+'make test' command tests the a.out on asgn4_19CS10045_test.c 
---
+++ Assignment 4 - Bison/asgn4_19CS10045_test.c
@@ -0,0 +1,71 @@
+enum LMVT { _ = 1, x_d = 2 - 3, H_eis00};
+enum _LMVT { xd = 2 };
+enum __LMVT;
+extern int x_e = 2/3 + 4*5;
+static float q_e = 2*3e7;
+int _f01 (int *a, unsigned int b[], double _c) {
+    signed int q = sizeof(a + 5), r = sizeof(b) / sizeof(int);
+    const int **x = &a; 
+    _c += (-0.4924e-2);
+    _c -= -.32;
+    _c = (float)(!_c);
+    b[2 - 1/1] *= 2;
+    return r + (5 + (+2) / 5 - (-7));
+}
+inline void _f02() { const char _f = '*'; }
+long long int _f03() {
+    const short x = -12;
+    float q = 3.56e-2;
+    signed int x_2 = 2&3, _y2, _z2;
+    x_2 = 1 + sizeof(++x_2);
+    x_2 *= 12;
+    x_2 /= 3;
+    x_2 = x_2 >> (1 + 1);
+    x_2 = (x_2 ++);
+    return (x_2 % 5) << 2;
+}
+void _f04() {
+    int _1 = 1, _2 = 10, _3 = 4;
+    _3 ^= 2; _2 |= 1;
+    if (_1 >= 2 && (_2 < 3) || (_3 <= 5)) {
+        _1--;
+        _2++;
+        --_3;
+    }
+    else ++_1; 
+    if (_1 != 2 + 4 * 5) _1 %= 2;
+    switch (_1) {
+        case 1 - 5 + 2: break;
+        case 23: _1 <<= 2;
+        default: _1 += 2;
+    }    
+}
+void _f05() {
+    unsigned int x = 2;
+    while (x < 3) {
+        if (x < 2) continue;
+        else x++;
+    }
+    do { --x; } while (x > 2/2 - 1);
+    for (x = 1; x <= 2; x++) if (x ^ 2) break;
+    for (int q = 1; q <= 1; q++) {x++;}
+    char f[50] = "Happy\n";
+}
+void _f06(double *q) {
+    double x_q = *q;
+    volatile int *x_q_1, y_q_1; 
+    unsigned int r_2, x_2, y_2, z_2;
+    r_2 = x_2 = y_2 = +3;
+    _Bool c = 0;
+}
+int main() {
+    int q = 1;
+    int *p = &q;
+    unsigned int _b20[4 + 5] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    double _f = (q == 1) ? -1 : -2.54;
+    _f01(p, _b20, _f);
+    _f02(); _f03(); 
+    _f04(); _f05();
+    _f06(&_f);
+    return 0;
+}
