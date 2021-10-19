3ddea54177fe0c518e46471c85de6406d28b21ea
Rajas Bhatt <rajas.bhatt@yahoo.com>
2021-10-19 06:58:24 +0530

    Added translater header and partially completed translator functions

---
+++ Assignment 5 - MIT/ass5_19CS30037_translator.cxx
@@ -0,0 +1,295 @@
+
+#include "ass5_19CS30037_translator.h"
+using namespace std;
+
+// Global Variables Q for array of all quads and ST for current symbol table
+QuadArray Q;
+Symboltable ST;
+
+// Basic Types and Sizes
+map <string, int> basic_sizes = {
+    {"char", SIZE_OF_CHAR}, 
+    {"float", SIZE_OF_FLOAT}, 
+    {"int", SIZE_OF_INT}, 
+    {"ptr", SIZE_OF_POINTER}, 
+    {"void", INT32_MAX}, 
+    {"bool", INT32_MAX}, 
+    {"block", 0}, 
+    {"func", 0}
+};
+
+// Construct a symbol type
+SymbolType::SymbolType(string _name, int _size, SymbolType * _next) 
+    :name (_name), size(_size), next(_next) {}
+
+// Get the space consumed by a data type
+int SymbolType::getSize() {
+    if (this->name == "arr") {
+        return this->size * this->next->getSize();
+    }
+    if (this->name == "void") {
+        // return error
+        cout << "Void has no size\n";
+        return basic_sizes[this->name];
+    }
+    return basic_sizes[this->name];
+}
+
+// Get the type of the symbol as a C++ string
+string SymbolType::getType() {
+    if (this->name == "arr") {
+        string special = "arr(";
+        special += conv_int2string(this->size);
+        special += ", ";
+        special += this->next->getType();
+        special += ")";
+        return special;
+    }
+    else if (this->name == "ptr") {
+        string special = "ptr(";
+        special += this->next->getType();
+        special += ")";
+        return special;
+    }
+    else return this->name;
+}
+
+// Print the type of the symbol
+void SymbolType::printType() {cout << this->getType() << "\n";}
+
+// Create a new entry in the symbol table
+Symbol::Symbol(string name, string type, string initval, int width, Symboltable * nested_table)
+    :name(name), type(new SymbolType(type, width)), initial_value(initval), nested_table(nested_table) {
+        this->size = this->type->getSize(); // Get the size
+        this->offset = 0;                   // Initial offset is 0
+}
+
+Symbol::Symbol(string _name) : name(_name) {}
+
+// Update the size of the symbol table entry
+// Used when conversion to different types
+void Symbol::update(SymbolType * new_type) {
+    this->type = new_type;              // Change the type
+    this->size = this->type->getSize(); // Get the size of the new type
+} 
+
+// Symboltable constructor
+Symboltable::Symboltable(string _name, Symboltable * _parent) 
+    : name(_name), parent(_parent), symbols({}) {
+        this->temporary_count = 0;
+    }
+
+// Lookup function in a Symboltable
+Symbol * Symboltable::lookup(string _id) {
+    // If the symbol is already found
+    for (Symbol c : this->symbols) {
+        if (c.name == _id) return &c;
+    }
+    // Else create a new symbol with this name
+    Symbol * ns = new Symbol(_id);
+    // Add to the list of symbols
+    this->symbols.push_back(*ns);
+    return &this->symbols.back();   // The latest symbol which was inserted
+}
+
+// Gentemp function to generate temporaries
+Symbol * Symboltable::gentemp() {
+    // Name of the temporary TEMP_x, x = count of the temporaries
+    string namx = "TEMP_";
+    namx += to_string(this->temporary_count++);
+    Symbol * ns = new Symbol(namx);
+
+    this->symbols.push_back(*ns);
+    return &this->symbols.back();   // The latest symbol
+}
+
+// Update function to update the symbol table offsets
+void Symboltable::update() {
+    int off = 0;
+    for (Symbol c : this->symbols) {
+        c.offset = off;
+        off += c.size;
+        if (c.nested_table != nullptr) {
+            c.nested_table->update();
+        }
+    }
+}
+
+// Symboltable print function
+void Symboltable::print() {
+    // Output the name and the parent
+    cout << "ST Name   : " << this->name << "\n";
+    cout << "ST Parent : ";
+    if (this->parent == nullptr) cout << "null\n";
+    else cout << this->parent->name << "\n";
+    cout << "\n++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
+    for (Symbol c : this->symbols) {
+        cout << "Name          : " << c.name << "\n";
+        cout << "Type          : " << c.type << "\n";
+        cout << "Initial Value : ";
+        if (c.initial_value == "") cout << "null\n";
+        else cout << c.initial_value << "\n";
+        cout << "Size          : " << c.size << "\n";
+        cout << "Offset        : " << c.offset << "\n";
+        cout << "Nested Table  : " << c.nested_table->name << "\n";
+    }
+    cout << "\n";
+}
+
+
+// Quad constructor
+Quad::Quad(string _res, string _op, string _arg1, string _arg2) 
+    : res(_res), op(_op), arg1(_arg1), arg2(_arg2) {}
+
+
+void Quad::print() {
+    // Get the quad operator
+    // Relational, arithmetic and shift operators involve two operands
+    vector <string> relop = {">", "<", ">=", "<=", "==", "!="};
+    vector <string> arithshift = {"+", "-", "*", "/", "%", "&", "|", "^", ">>", "<<"};
+
+    // Only involve one argument
+    vector <string> unary1 = {"=&", "=*", "*="};
+    vector <string> unary2 = {"!", "~"};
+
+    bool match = false;
+    
+    // Arithmetic and Relational Operators
+    for (auto a : arithshift) if (op == a) {
+        cout << res << " = " << arg1 << " " << op << " " << arg2;
+        match = true;
+    }
+    for (auto r : relop) if (op == r) {
+        cout << "if " << arg1 << " " << op << " " << arg2 << " goto " << res;
+        match = true;
+    }
+    // Unary operators print
+    for (auto u : unary1) if (op == u) {
+        cout << res << op[0] << " " << op[1] << " " << arg1;
+        match = true;
+    }
+    for (auto u : unary2) if (op == u) {
+        cout << res << " = " << op << " " << arg1;
+        match = true;
+    }
+    if (match) cout << "\n"; 
+    else {
+        // Goto, return and parameter passing
+        if (op == "goto") cout << "goto " << res;
+        else if (op == "return") cout << "return " << res;
+        else if (op == "param") cout << "param " << res;
+
+        // Assignment Operator
+        else if (op == "=") cout << res << " = " << arg1;
+        
+        // Unary minus
+        else if (op == "uminus") cout << res << "= -" << arg1;
+        
+        // Array Access
+        else if (op == "=[]") cout << res << " = " << arg1 << "[" << arg2 << "]";
+        else if (op == "[]=") cout << res << "[" << arg1 << "] = " << arg2;
+
+        // Function Call (Format ans = call func, 2)
+        else if (op == "call") cout << res << " = " << "call " << arg1 << ", " << arg2;
+        // Label ? 
+
+        else cout << "No matching operator found";
+
+        cout << "\n";
+    }
+
+}
+
+
+// Quadarray functions
+// Insert function into quadarray
+void QuadArray::insert(Quad q) {
+    this->quads.push_back(q);
+}
+
+// Print the entire QuadArray
+void QuadArray::print() {
+    cout << "------------------TAC-----------------\n";
+    for (Quad q : this->quads) {
+        q.print();
+    }
+}
+
+// Static Emit functions to add to the global quad array
+void emit(string _op, string _res, int _arg1, string _arg2) {
+    Quad * nq = new Quad(_res, _op, conv_int2string(_arg1), _arg2);
+    Q.insert(*nq);
+}
+
+void emit(string _op, string _res, float _arg1, string _arg2) {
+    Quad * nq = new Quad(_res, _op, conv_float2sring(_arg1), _arg2);
+    Q.insert(*nq);
+}
+
+void emit(string _op, string _res, string _arg1, string _arg2) {
+    Quad * nq = new Quad(_res, _op, _arg1, _arg2);
+    Q.insert(*nq);
+}
+
+
+// Makelist
+vector <int> makelist (int i) {
+    return vector <int> (1, i);
+}
+
+// Mergelist
+vector <int> merge (vector <int> &p1, vector <int> &p2) {
+    vector <int> v3;
+    copy(p1.begin(), p1.end(), v3);
+    for (auto p : p2) v3.push_back(p);
+    return v3;
+}
+
+// Backpatch
+void backpatch(vector<int> &p1, int i) {
+    string s = conv_int2string(i);    
+    // Add i as the target label for all quads on the list in p1
+    for (auto p : p1) {
+        Q.quads[p].res = s;
+    }
+}
+
+// Conversion of int type expression into bool type expression as done in the class
+void conv_int2bool(Expression &E) {
+    // Sanity Checking to ensure that the expression is not already boolean
+    if (E.type != "bool") {
+        E.falselist = makelist(nextinstr());    // Update the falselist
+        emit("==", "", E.loc->name, "0");       // Goto statement
+        E.truelist = makelist(nextinstr());     // Update truelist
+        emit("goto","");                        // Wait for backpatching
+    }
+}
+
+// Conversion of bool type expression into int type expression 
+void conv_bool2int(Expression &E) {
+    // Sanity checking to ensure that expression is boolean
+    if (E.type == "bool") {
+        SymbolType * ts = new SymbolType("int");
+        E.loc = gentemp(&ST, ts);
+        backpatch(E.truelist, nextinstr());
+        emit("==", E.loc->name, "true", "");
+        int nx = nextinstr() + 1;
+        string strx = conv_int2string(nx);
+        emit("goto", strx);
+        backpatch(E.falselist, nextinstr());
+        emit("=", E.loc->name, "false");
+    }
+}
+
+// Check the types of E1 and E2 and convert if types are compatible
+
+// Go to the next instruction
+/* Next instruction is the index which is one larger than the largest
+/* index in the quad array. 
+/* However, since C++ vectors follow zero based indexing, the size() of the
+/* vector is one larger than the largest index
+/* Hence the next instruction is nothing but the size of the vector */
+int nextinstr() {
+    return Q.quads.size();
+}
+
---
+++ Assignment 5 - MIT/ass5_19CS30037_translator.h
@@ -0,0 +1,161 @@
+#ifndef _TRANSLATOR_H_
+#define _TRANSLATOR_H_
+
+#define SIZE_OF_CHAR    1
+#define SIZE_OF_INT     4
+#define SIZE_OF_FLOAT   8
+#define SIZE_OF_POINTER 4
+
+#include <string> 
+#include <vector>
+#include <iostream>
+#include <map> 
+
+extern QuadArray Q;             // The program array of quads
+extern Symboltable ST;          // The current symbol table
+
+// The type of an object in the symbol table defined in a recursive manner
+class SymbolType {
+private:
+    std::string name;       // Type name
+    int size;               // Size of the current type
+    SymbolType * next;      // Pointer to the type deeper in the recursive tree
+public:
+    SymbolType(std::string _name, int _size = 1, SymbolType * _next = nullptr);
+    std::string getType();// Print the type depicting the string
+    void printType();
+    int getSize();          // Get the total size 
+};
+
+// A symbol which is an entry of the symbol table
+class Symbol {
+public:
+    std::string name;           // Name of the symbol (string)
+    SymbolType * type;          // Type of the symbol (can be a list e.g. arr(10, arr(10, int)) )
+    std::string initial_value;  // Initial value of the symbol
+    int size;                   // Size of the symbol 
+    int offset;                 // Offset of the symbol
+    Symboltable * nested_table; // Pointer to the nested symbol table
+
+    Symbol(std::string name);
+    Symbol(std::string name, 
+        std::string type, 
+        std::string initval = "", 
+        int width = 0, 
+        Symboltable * nested_table = nullptr);        // Make new entry
+    
+    void update (SymbolType *);                       // Change the type
+
+};
+
+// The symboltable which is a list of symbols
+class Symboltable{
+public:
+    std::vector<Symbol> symbols;        // The symbols in the symbol table
+    std::string name;                   // Name of the symbol table
+    Symboltable * parent;               // Parent table of the symbol table
+    int temporary_count;                // Counts the number of temporaries
+    Symboltable(std::string _name, Symboltable * _parent = nullptr);    // New symbol table
+    Symboltable();                      // Empty constructor
+    Symbol * lookup(std::string id);     // Lookup a given symbol using name
+    Symbol * gentemp();                  // Generate a new temporary and insert it into the symbol table
+    void print();                        // Print the symbol table
+    void update();                       // Update the symbol table
+};
+
+// The Stack of the Symboltables on which the search will be carried out
+class SymtabStack {
+private:
+    std::vector<Symboltable *> tables;
+    Symboltable * ST;
+public:
+    Symboltable * current();    
+    Symboltable * global ();    // First symbol table
+    void updateCurrent (Symboltable * newST); // Update current symbol table
+};
+
+// Definition of the Quad
+class Quad {
+private:
+    std::string res;                    // Result
+    std::string op;                     // Opcode
+    std::string arg1;                   // Argument 1
+    std::string arg2;                   // Argument 2
+
+public:
+    // Constructor
+    Quad (std::string _res, std::string _op, std::string _arg1 = "", std::string _arg2 = "");
+    void print();                       // Print the quad
+};
+
+// Definition of the Quad Array
+class QuadArray {
+private:
+    std::vector<Quad> quads;            // All the quads stored in the array
+public:
+    void insert (Quad q);               // Insert a quad into the quad array
+    void print();                       // Print all the quads
+};
+
+// Static emit functions which emit quads
+void emit (std::string _res, std::string _op, std::string _arg1 = "", std::string _arg2 = "");
+void emit (std::string _res, std::string _op, int _arg1, std::string _arg2 = "");
+void emit (std::string _res, std::string _op, float _arg1, std::string _arg2 = "");
+
+
+// Expressions
+struct Expression {
+    Symbol * loc;                       // Pointer to the symbol table entry
+    std::string type;                   // Type of the expression
+    std::vector <int> nextlist;         // Nextlist for statement expressions
+    std::vector <int> truelist;         // Truelist for boolean expressions
+    std::vector <int> falselist;        // Falselist for boolean expressions
+
+};
+
+//Statement Block
+struct Next {
+    std::vector <int> nextlist;              // Dangling exit nextlist
+};
+
+// Arrays in Expression
+struct Array {
+    Symbol * loc;  
+    Symbol * array;
+    SymbolType * type;
+};
+
+// Global Functions
+// Gentemp function, to generate a temporary and add it to the given
+// symbol table and return an address to the symbol
+Symbol * gentemp (Symboltable * s, SymbolType * type = new SymbolType("int"));
+
+/* makelist : Create a new list comprising only i and return a pointer
+ * to the newly created list */
+std::vector <int> makelist (int i);
+// merge: Merge two lists p1 and p2 and return a pointer to the merged list
+std::vector <int> merge (std::vector <int> &p1, std::vector <int> &p2);
+// backpatch: Insert i as the target label for each quads on the list pointed to by p
+void backpatch (std::vector <int> &p, int i);
+
+// Typecheck and conversion functions
+void typecheck (Expression &E1, Expression &E2);
+
+// Conversion of int and float to string
+inline std::string conv_int2string (int a) {return to_string(a);}
+inline std::string conv_float2sring (float a) {return to_string(a);}
+// Conversion of string to float and int
+inline int conv_string2int (std::string s) {return stoi(s);}
+inline float conv_string2float (std::string s) {return stof(s);}
+// Convert integer to float by multiplying by 1.0
+inline float conv_int2float (int a) {return a * 1.0;}
+
+// Integer to Boolean conversion in Conditional Expressions
+void conv_int2bool (Expression &E);
+void conv_bool2int (Expression &E);
+
+
+// Next instruction address
+int nextinstr();
+
+#endif // _TRANSLATOR_H_
